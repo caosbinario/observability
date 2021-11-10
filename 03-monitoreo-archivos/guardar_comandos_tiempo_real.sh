b2ce92c4d7bcbab73ccb90f98e6fc45bc0900085
Caos Binario <caosbinario@gmail.com>
2021-11-10 20:38:37 -0300

    codigo video 03

---
+++ 03-monitoreo-archivos/guardar_comandos_tiempo_real.sh
@@ -0,0 +1,5 @@
+# en comandos de "echo"
+echo '' >> ~/.bashrc
+echo '#guardar los comandos en tiempo real' >> ~/.bashrc
+echo 'shopt -s histappend' >> ~/.bashrc
+echo 'PROMPT_COMMAND="history -a;$PROMPT_COMMAND"' >> ~/.bashrc
